c599203a8db47dbf00f988d5fe90b3f2093d8626
Kelly Lee <79069234+Leekkelly@users.noreply.github.com>
2024-08-17 20:19:27 +1000

    project updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const navList = document.getElementById('nav-list');
 
 function toggleButton() {
     navList.classList.toggle('show')
-
+    
 }
 
 hamburgerButton.addEventListener('click', toggleButton);
